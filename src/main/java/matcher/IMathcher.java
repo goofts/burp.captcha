0de2434dc7979f6626e2568d2c25e41293ced06c
倾枭 <goofts@zl.com>
2021-02-25 10:19:54 +0800

    add burpsuite.jar

--- src/main/java/matcher/IMathcher.java
+++
@@ -1,8 +0,0 @@
-package matcher;
-
-import entity.MatchResult;
-
-public interface IMathcher {
-    public MatchResult match(String strResp, String keyword);
-    public String buildKeyword(String strResp,String value);
-}
